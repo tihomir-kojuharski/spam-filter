eacd2bc9546d12f5f5df7551053cc74cd92e5a26
Vladimir Kuzmov <vladimir.kuzmov@gmail.com>
2017-06-24 15:45:07 +0300

    accuracy added

--- pipeline.py
+++ pipeline.py
@@ -7,7 +7,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing.data import MinMaxScaler
 from sklearn.svm import SVC
-import numpy as np
 
 from features.character_based.alpha_ratio import AlphaRatio
 from features.character_based.digit_ratio import DigitRatio
@@ -54,17 +53,17 @@ def get_dataset():
 
 def get_features(train):
     features = [
-        # ('word_counts', WordCounts(train)),
-        # ('number_of_characters', NumberOfCharacters()),
-        # ('alpha_ratio', AlphaRatio()),
-        # ('digit_ratio', DigitRatio()),
-        # ('whitespace_ratio', WhitespaceRatio()),
-        # ('special_chars_ratio', SpecialCharsRatio()),
-        # ('number_of_words', NumberOfWords()),
-        # ('short_words_ratio', ShortWordsRatio()),
-        # ('average_word_len', AverageWordLen()),
-        # ('unique_words_ratio', UniqueWordsRatio()),
-        # ('spam_words', SpamWords()),
+        ('word_counts', WordCounts(train)),
+        ('number_of_characters', NumberOfCharacters()),
+        ('alpha_ratio', AlphaRatio()),
+        ('digit_ratio', DigitRatio()),
+        ('whitespace_ratio', WhitespaceRatio()),
+        ('special_chars_ratio', SpecialCharsRatio()),
+        ('number_of_words', NumberOfWords()),
+        ('short_words_ratio', ShortWordsRatio()),
+        ('average_word_len', AverageWordLen()),
+        ('unique_words_ratio', UniqueWordsRatio()),
+        ('spam_words', SpamWords()),
         ('flesch_reading_score', FleschReadingEase()),
     ]
     return features
@@ -94,9 +93,8 @@ def run_classifiers(test, train):
     pipeline = get_pipeline(get_features(train))
     X_train = pipeline.fit_transform(train)
     trainLabels = [instance['is_spam'] for instance in train]
-
     testLabels = [instance["is_spam"] for instance in test]
-    # testLabels = np.array([[instance["is_spam"]] for instance in test])
+
     # get the features for the test set
     X_test = pipeline.fit_transform(test)
 
@@ -116,11 +114,13 @@ def run_classifiers(test, train):
         confusionMatrix = confusion_matrix(testLabels, predictedLabels)
         precision = confusionMatrix[1, 1] / (confusionMatrix[1, 1] + confusionMatrix[0, 1])
         recall = confusionMatrix[1,1] / (confusionMatrix[1,1] + confusionMatrix[1,0])
+        accuracy = ((confusionMatrix[0, 0] + confusionMatrix[1, 1]) / len(predictedLabels)) * 100
 
         print("\nConfusion matrix:\n{0}\n".format(confusionMatrix))
         print("\nPrecision: {0}\n".format(precision))
         print("Recall: {0}\n".format(recall))
         print("F1: {0}\n".format(2 * (precision * recall) / (precision + recall)))
+        print("Accuracy: {0}\n".format(round(accuracy, 2)))
 
 
 if __name__ == "__main__":
