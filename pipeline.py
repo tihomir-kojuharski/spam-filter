40e7f1225a436cc7742ac101867d926812c7ec57
Vladimir Kuzmov <vladimir.kuzmov@gmail.com>
2017-07-05 16:25:35 +0300

    results tables added

--- pipeline.py
+++ pipeline.py
@@ -1,17 +1,39 @@
 import datetime
 import os
-
 import numpy as np
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing.data import MinMaxScaler
 
-from features.word_based.word_counts import WordCounts
+# classifiers
 from model import ToMatrix
 from neural_network import NeuralNetwork
 from twinsvm.TVSVM import TwinSVMClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing.data import MinMaxScaler
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis, LinearDiscriminantAnalysis
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+# features
+from features.word_based.word_counts import WordCounts
+from features.character_based.number_of_characters import NumberOfCharacters
+from features.character_based.alpha_ratio import AlphaRatio
+from features.character_based.digit_ratio import DigitRatio
+from features.character_based.whitespace_ratio import WhitespaceRatio
+from features.character_based.special_chars_ratio import SpecialCharsRatio
+from features.word_based.number_of_words import NumberOfWords
+from features.word_based.short_words_ratio import ShortWordsRatio
+from features.word_based.average_word_len import AverageWordLen
+from features.word_based.unique_words_ratio import UniqueWordsRatio
+from features.word_based.spam_words import SpamWords
+from features.flesh_reading_score import FleschReadingEase
+from sklearn.neural_network import MLPClassifier
+
 
 dataset_dir = "enron-dataset"
 
@@ -43,18 +65,18 @@ def get_dataset():
 
 def get_features(train):
     features = [
-        ('word_counts', WordCounts(train)),
-        # ('number_of_characters', NumberOfCharacters()),
-        # ('alpha_ratio', AlphaRatio()),
-        # ('digit_ratio', DigitRatio()),
-        # ('whitespace_ratio', WhitespaceRatio()),
-        # ('special_chars_ratio', SpecialCharsRatio()),
-        # ('number_of_words', NumberOfWords()),
-        # ('short_words_ratio', ShortWordsRatio()),
-        # ('average_word_len', AverageWordLen()),
-        # ('unique_words_ratio', UniqueWordsRatio()),
-        # ('spam_words', SpamWords()),
-        # ('flesch_reading_score', FleschReadingEase()),
+        # ('word_counts', WordCounts(train)),
+        ('number_of_characters', NumberOfCharacters()),
+        ('alpha_ratio', AlphaRatio()),
+        ('digit_ratio', DigitRatio()),
+        ('whitespace_ratio', WhitespaceRatio()),
+        ('special_chars_ratio', SpecialCharsRatio()),
+        ('number_of_words', NumberOfWords()),
+        ('short_words_ratio', ShortWordsRatio()),
+        ('average_word_len', AverageWordLen()),
+        ('unique_words_ratio', UniqueWordsRatio()),
+        ('spam_words', SpamWords()),
+        ('flesch_reading_score', FleschReadingEase()),
     ]
     return features
 
@@ -74,30 +96,16 @@ def run_classifiers(test, train):
     nnClassifier = NeuralNetwork(activation='sigmoid')
 
     params3 = {'Epsilon1': 0.1, 'Epsilon2': 0.1, 'C1': 1, 'C2': 1, 'kernel_type': 0, 'kernel_param': 1, 'fuzzy': 0}
-    params4 = {'Epsilon1': 0.1, 'Epsilon2': 0.1, 'C1': 1, 'C2': 1,'kernel_type':0,'kernel_param': 1,'fuzzy' :1}
-    params5 = {'Epsilon1': 0.1, 'Epsilon2': 0.1, 'C1': 1, 'C2': 1,'kernel_type':1,'kernel_param': 1,'fuzzy' :0}
     params6 = {'Epsilon1': 0.1, 'Epsilon2': 0.1, 'C1': 1, 'C2': 1,'kernel_type':1,'kernel_param': 1,'fuzzy' :1}
-    params7 = {'Epsilon1': 0.1, 'Epsilon2': 0.1, 'C1': 1, 'C2': 1,'kernel_type':2,'kernel_param': 2,'fuzzy' :0}
-    params8 = {'Epsilon1': 0.1, 'Epsilon2': 0.1, 'C1': 1, 'C2': 1,'kernel_type':2,'kernel_param': 2,'fuzzy' :1}
-    params9 = {'Epsilon1': 0.1, 'Epsilon2': 0.1, 'C1': 1, 'C2': 1, 'kernel_type': 3, 'kernel_param': 2, 'fuzzy': 0}
-    params10 = {'Epsilon1': 0.1, 'Epsilon2': 0.1, 'C1': 1, 'C2': 1, 'kernel_type': 3, 'kernel_param': 2, 'fuzzy': 1}
-
 
     classifiers = [
         # SVC(kernel="linear", C=0.025),
         # SVC(gamma=2, C=1),
-        TwinSVMClassifier(**params3),
-        TwinSVMClassifier(**params4),
-        TwinSVMClassifier(**params5),
-        TwinSVMClassifier(**params6),
-        TwinSVMClassifier(**params7),
-        TwinSVMClassifier(**params8),
-        TwinSVMClassifier(**params9),
-        TwinSVMClassifier(**params10),
-        MultinomialNB(),
+        # TwinSVMClassifier(**params3),
+        # TwinSVMClassifier(**params6),
+        # MultinomialNB(),
         # KNeighborsClassifier(1),
-        # nnClassifier,
-        # GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
+        nnClassifier,
         # DecisionTreeClassifier(max_depth=5),
         # RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
         # MLPClassifier(alpha=1),
@@ -111,19 +119,19 @@ def run_classifiers(test, train):
     #  get pipeline of features and generate them
     pipeline = get_pipeline(get_features(train))
 
-    # X_train = np.load('enron_train_features_matrix.npy')
-    # trainLabels = np.load('enron_train_labels.npy')
+    # X_train = np.load('enron_word_cnt_train_features_matrix.npy')
+    # trainLabels = np.load('enron_word_cnt_train_labels.npy')
     X_train = pipeline.fit_transform(train)
     trainLabels = [instance['is_spam'] for instance in train]
-    np.save('enron_train_features_matrix.npy', X_train)
-    np.save('enron_train_labels.npy', trainLabels)
+    # np.save('enron_train_features_matrix.npy', X_train)
+    # np.save('enron_train_labels.npy', trainLabels)
 
+    # X_test = np.load('enron_word_cnt_test_features_matrix.npy')
+    # testLabels = np.load('enron_word_cnt_test_labels.npy')
     X_test = pipeline.fit_transform(test)
     testLabels = [instance["is_spam"] for instance in test]
-    # X_test = np.load('enron_test_features_matrix.npy');
-    # testLabels = np.load('enron_test_labels.npy');
-    np.save('enron_test_features_matrix.npy', X_test)
-    np.save('enron_test_labels.npy', testLabels)
+    # np.save('enron_test_features_matrix.npy', X_test)
+    # np.save('enron_test_labels.npy', testLabels)
 
     nnClassifier.X_test = np.array(X_test)
     nnClassifier.Y_test = np.array(testLabels)
